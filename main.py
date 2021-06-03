087bac463bbb7713f549026c4c753027482fe277
AcornBerries <>
2021-06-03 11:08:05 +0000

    -Cleaned up distance calculation function -Implemented basement door

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import math
 
 encouraging_message = ("Don't be scared, you can do it!", "If you don't try, you can't win!", "What's the worst that can happen? Face your fears!")
 random_int_for_e_m = random.randint(1,3) - 1
+
+
+"""
 in_basement = True
 
 #comment
@@ -25,7 +28,7 @@ def accidental_location_revisit_check(record_dict, selection):
     else:
       return False
 
-"""
+
 #introduction
 print("Welcome to the Choose Your Own Adventure Game!")
 print("And so the story begins...")
@@ -110,8 +113,6 @@ while in_basement:
 # Kitchen: Stove and Sink
 locations = (("living room", ("tv", 5,8), ("couch",5,11)), ("dining room", ("stack of chairs", 22,1), ("table", 17,3)), ("bathroom", ("bathroom sink", 3,1), ("toilet", 1,1)), ("kitchen", ("stove", 15,11), ("kitchen sink", 23,10)))
 
-ladder_loc = (random.randint(0, 3), random.randint(1, 2))#first int is the room index, second int is the object index
-
 current_loc = [0, 1]#the first int is the index of the room, and the second is the index of the object.
 
 #helper functions for the main floor
@@ -127,23 +128,29 @@ def hotcold_get_new_obj():
   
   current_loc[1] = int(input("Out of the above objects, which would you like to check? (enter the number) "))
 
-def current_distance_to_ladder():
-  x_diff = abs(locations[ladder_loc[0]][ladder_loc[1]][1] - locations[current_loc[0]][current_loc[1]][1])
-  y_diff = abs(locations[ladder_loc[0]][ladder_loc[1]][2] - locations[current_loc[0]][current_loc[1]][2])
-  return math.sqrt(x_diff^2 + y_diff^2)
+def get_room_obj_coord(room_obj_idxs):
+  x_value = locations[room_obj_idxs[0]][room_obj_idxs[1]][1]
+  y_value = locations[room_obj_idxs[0]][room_obj_idxs[1]][2]
+  return (x_value, y_value)
+
+def distance_coord_to_ladder(coord_pair):
+  x_diff = ladder_coords[0] - coord_pair[0]
+  y_diff = ladder_coords[1] - coord_pair[1]
+  return math.sqrt(x_diff**2 + y_diff**2)
+
+#setting ladder object location and retrieving coordinates
+ladder_loc = (random.randint(0, 3), random.randint(1, 2))#first int is the room index, second int is the object index
+ladder_coords = get_room_obj_coord(ladder_loc)
 
 #MAIN FLOOR CODE
 print("Congratulations!  You have made it to the main floor!")
 print("You are now faced with an open-concept house layout.")
 
-#initializing the game
+distance_old = distance_coord_to_ladder((12, 0,))#(12, 0) is the coordinate pair of the basement door
 hotcold_get_new_room()
-
-#the distance is between the ladder and the object
-distance_old = current_distance_to_ladder()
-
 hotcold_get_new_obj()
-distance_new = current_distance_to_ladder()#not distance new, distance current
+#this variable is the current distance (after moving to the current object)
+distance_new = distance_coord_to_ladder(get_room_obj_coord(current_loc))
 
 while True:
 #is the current object the ladder location?
@@ -160,18 +167,25 @@ while True:
   else:
     print("You have not moved.")
   
+  #TO MAKE PROGRAM PROCESS MORE CLEAR WHILE PROGRAMMING IS INCOMPLETE
+  print("distance_new:", distance_new)
+  print("distance_old:", distance_old)
+  print("current_loc:", current_loc)
+
   while True:
-    y_n_check = input("Would you like to check another object in this room: ")
-    if "yes" in y_n_check:
+    y_n_check = input("Would you like to check another object in this room (y/n): ")
+    if "y" in y_n_check:
+      distance_old = distance_coord_to_ladder(get_room_obj_coord(current_loc))
       hotcold_get_new_obj()
+      distance_new = distance_coord_to_ladder(get_room_obj_coord(current_loc))
       break
     else:
-      y_n_other_room = input("Would you like to check another object in another room: ")
-      if "yes" in y_n_check:
-        distance_old = current_distance_to_ladder()
+      y_n_other_room = input("Would you like to go to another room then? (y/n): ")
+      if "y" in y_n_other_room:
+        distance_old = distance_coord_to_ladder(get_room_obj_coord(current_loc))
         hotcold_get_new_room()
         hotcold_get_new_obj()
-        distance_new = current_distance_to_ladder()
+        distance_new = distance_coord_to_ladder(get_room_obj_coord(current_loc))
         break
       else:
         print(encouraging_message)
@@ -183,4 +197,3 @@ while True:
 
 #The ladder location is at a specific thing in the main floor (bathroom sink, chairs, etc.)
 print("CREAK. Whoosh. A trapdoor opens above you, and a ladder falls down, missing you by a hair.")
-
