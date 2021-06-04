cfcac8ff16076dd78b3df078642da778d658ac82
AcornBerries <>
2021-06-04 10:13:20 +0000

    Added another object to each room, inserted map into code

--- main.py
+++ main.py
@@ -2,21 +2,13 @@ import random
 import time
 import math
 
+
 def encouragingMessage():
   encouraging_message = ("Don't be scared, you can do it!", "If you don't try, you can't win!", "What's the worst that can happen? Face your fears!")
   random_int_for_e_m = random.randint(1,3) - 1
   print(encouraging_message[random_int_for_e_m])
 
-
-
-
 """
-in_basement = True
-
-#comment
-#not yet implemented within the code.
-basement_visiting_record = {"yellow": False, "red": False, "blue": False}
-
 #helper functions for the ground floor
 def back_to_the_hallway():
   stay_go_back = input("Would you like to go back to the hallway? (y/n)").lower()
@@ -32,6 +24,10 @@ def accidental_location_revisit_check(record_dict, selection):
     else:
       return False
 
+in_basement = True
+
+#not yet implemented within the code.
+basement_visiting_record = {"yellow": False, "red": False, "blue": False}
 
 #introduction
 print("Welcome to the Choose Your Own Adventure Game!")
@@ -115,7 +111,7 @@ while in_basement:
 # Dining Room: Stack of Chairs and table
 # Bathroom: Sink and Toilet
 # Kitchen: Stove and Sink
-locations = (("living room", ("tv", 5,8), ("couch",5,11)), ("dining room", ("stack of chairs", 22,1), ("table", 17,3)), ("bathroom", ("bathroom sink", 3,1), ("toilet", 1,1)), ("kitchen", ("stove", 15,11), ("kitchen sink", 23,10)))
+locations = (("LIVING ROOM", ("Tv", 5,8), ("Couch",5,11), ("Carpet", 9,2)), ("DINING ROOM", ("Stack of Chairs", 22,1), ("Table", 17,3), ("Sideboard", 22,5)), ("BATHROOM", ("Bathroom sink", 3,1), ("Toilet", 1,1), ("Cupboard", 4,1)), ("KITCHEN", ("Stove", 15,11), ("Kitchen sink", 23,10), ("Fridge", 19,11)))
 
 current_loc = [0, 1]#the first int is the index of the room, and the second is the index of the object.
 
@@ -143,14 +139,43 @@ def distance_coord_to_ladder(coord_pair):
   return math.sqrt(x_diff**2 + y_diff**2)
 
 #setting ladder object location and retrieving coordinates
-ladder_loc = (random.randint(0, 3), random.randint(1, 2))#first int is the room index, second int is the object index
+ladder_loc = (random.randint(0, 3), random.randint(1, 3))#first int is the room index, second int is the object index
 ladder_coords = get_room_obj_coord(ladder_loc)
 
 #MAIN FLOOR CODE
 print("Congratulations!  You have made it to the main floor!")
 print("You are now faced with an open-concept house layout.")
 
-distance_old = distance_coord_to_ladder((12, 0,))#(12, 0) is the coordinate pair of the basement door
+print("                                           Ground Floor Map")
+print(" ")
+print(" 12┌───────────────────────────────────────────────────────────────────────────────────────────────┐")
+print("   │                                               |                                               │")
+print(" 11│   .   .   .   .  (1)  .   .   .   .   .   .       .   .  (0)  .   .   .  (2)  .   .   .   .   │")
+print("   │                 Couch                         |         Stove          Fridge                 │")
+print(" 10│   .   .   .   .   .   .   .   .   .   .   .       .   .   .   .   .   .   .   .   .   .  (1)  │")
+print("   │                                               |                                           Sink│")
+print(" 9 │   .   .   .   .   .   .   .   .   .   .   .       .   .   .   .   .   .   .   .   .   .   .   │")
+print("   │                                               |                    KITCHEN                    │")
+print(" 8 │   .   .   .   .  (0)TV.   .   .   .   .   .       .   .   .   .   .   .   .   .   .   .   .   │")
+print("   │                                               |                                               │")
+print(" 7 │   .   .   .   .   .   .   .   .   .   .   .       .   .   .   .   .   .   .   .   .   .   .   │")
+print("   │                 LIVING ROOM                   |                                               │")
+print(" 6 │   .   .   .   .   .   .   .   .   .   .   .    _ _._ _._ _._ _._ _._ _._ _._ _._ _._ _._ _._ _│")
+print("   │                                               |                                               │")
+print(" 5 │   .   .   .   .   .   .   .   .   .   .   .       .   .   .   .   .   .   .   .   .  (2)  .   │")
+print("   │                                               |            DINING ROOM           Sideboard    │")
+print(" 4 │   .   .   .   .   .   .   .   .   .   .   .       .   .   .   .   .   .   .   .   .   .   .   │")
+print("   │                                               |                                               │")
+print(" 3 ├───────────────────┐   .   .   .   .   .   .       .   .   .   .  (1)  .   .   .   .   .   .   │")
+print("   │     BATHROOM      │            Carpet         |                 Table                         │")
+print(" 2 │   .   .   .   .   │   .   .   .  (2)  .   .       .   .   .   .   .   .   .   .   .   .   .   │")
+print("   │              Cup- │                           |                                     Stack     │")
+print(" 1 │  (0)  .  (1) (2)  │   .   .   .   .   .   .       .   .   .   .   .   .   .   .   .  (0)  .   │")
+print("   │Toilet  Sink  board│                         START                                 of Chairs   │")
+print(" 0 └───────────────────┴──────────────────────────(┼)──────────────────────────────────────────────┘")
+print("       1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24")
+
+distance_old = distance_coord_to_ladder((12, 0,))#(12, 0) is the coordinate pair of the basement door (START)
 hotcold_get_new_room()
 hotcold_get_new_obj()
 #this variable is the current distance (after moving to the current object)
